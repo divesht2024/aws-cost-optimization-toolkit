5d0fcfbd28dbe48baea3bd80681ded58188574bf
Divesh Tayade <162043849+divesht2024@users.noreply.github.com>
2026-06-02 01:47:30 +0530

    Update stop_ec2.py

--- ec2-scheduler/stop_ec2.py
+++ ec2-scheduler/stop_ec2.py
@@ -1,1 +1,19 @@
 
+import boto3
+
+ec2 = boto3.client("ec2")
+
+def lambda_handler(event, context):
+
+    instance_id = "i-81d7613317055206"
+
+    ec2.stop_instances(
+        InstanceIds=[instance_id]
+    )
+
+    print(f"Stopped EC2 instance: {instance_id}")
+
+    return {
+        "statusCode": 200,
+        "body": "EC2 stopped successfully"
+    }

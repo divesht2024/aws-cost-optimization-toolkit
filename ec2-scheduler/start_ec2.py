23e1ebe9fad77c48ebc5b83a6f0862ddf84b1a79
Divesh Tayade <162043849+divesht2024@users.noreply.github.com>
2026-06-02 01:46:47 +0530

    Update start_ec2.py

--- ec2-scheduler/start_ec2.py
+++ ec2-scheduler/start_ec2.py
@@ -1,1 +1,18 @@
+import boto3
 
+ec2 = boto3.client('ec2')
+
+def lambda_handler(event, context):
+
+    instance_id = "i-01d7b13317c5f520b"
+
+    ec2.start_instances(
+        InstanceIds=[instance_id]
+    )
+
+    print(f"Started EC2 instance: {instance_id}")
+
+    return {
+        "statusCode": 200,
+        "body": "EC2 started successfully"
+    }
